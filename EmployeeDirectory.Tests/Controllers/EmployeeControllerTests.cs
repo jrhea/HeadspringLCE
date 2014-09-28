405b736897218e21cea753af1d6b21aac8d070a7
jonny rhea <jrhea1980@hotmail.com>
2014-09-28 18:24:08 -0500

    Adding Unit Tests

---
+++ EmployeeDirectory.Tests/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeDirectory;
+using EmployeeDirectory.Controllers;
+using EmployeeData;
+
+namespace EmployeeDirectory.Tests.Controllers
+{
+    [TestClass]
+    public class ViewTests
+    {
+        [TestMethod]
+        public void IndexView()
+        {
+            // Arrange
+            EmployeesController controller = new EmployeesController("1001");
+
+            // Act
+            ViewResult result = controller.Index("Last Name", "", "L",1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DetailsView()
+        {
+            // Arrange
+            EmployeesController controller = new EmployeesController("1001");
+
+            // Act
+            ViewResult result = controller.Details(1001) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CreateView()
+        {
+            // Arrange
+            EmployeesController controller = new EmployeesController("1001");
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void EditView()
+        {
+            // Arrange
+            EmployeesController controller = new EmployeesController("1001");
+
+            // Act
+            ViewResult result = controller.Edit(1001) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DeleteView()
+        {
+            // Arrange
+            EmployeesController controller = new EmployeesController("1001");
+
+            // Act
+            ViewResult result = controller.Delete(1001) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ EmployeeDirectory.Tests/Service/EmployeeServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeDirectory;
+using EmployeeDirectory.Controllers;
+using EmployeeData;
+
+namespace EmployeeDirectory.Tests.Service
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        private static int _testId;
+        [TestMethod]
+        public void CreateTest()
+        {
+            EmployeeServiceRef.EmployeeServiceClient esc = new EmployeeServiceRef.EmployeeServiceClient();
+            Employee employee = new Employee
+            {
+                FirstName = "Theodore",
+                LastName = "Kerabatsos",
+                HomePhone = "123456789",
+                JobTitle = "Janitor",
+                Location = "Dallas",
+                RoleId = 1,
+                Email = "outofyouremelement@foo.com"
+
+            };
+            _testId = esc.CreateEmployee(employee);
+            Assert.IsTrue(_testId != -1);
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            EmployeeServiceRef.EmployeeServiceClient esc = new EmployeeServiceRef.EmployeeServiceClient();
+            Employee employee = esc.GetEmployee(_testId);
+            Assert.IsNotNull(employee);
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            EmployeeServiceRef.EmployeeServiceClient esc = new EmployeeServiceRef.EmployeeServiceClient();
+            List<Employee> employees = esc.GetAllEmployees();
+            Assert.IsNotNull(employees);
+            Assert.IsTrue(employees.Count > 0);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            EmployeeServiceRef.EmployeeServiceClient esc = new EmployeeServiceRef.EmployeeServiceClient();
+            Employee employee = esc.GetEmployee(_testId);
+            employee.FirstName = "Donny";
+            esc.UpdateEmployee(employee);
+            employee = esc.GetEmployee(_testId);
+            Assert.IsNotNull(employee);
+            Assert.IsTrue(employee.FirstName == "Donny");
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            EmployeeServiceRef.EmployeeServiceClient esc = new EmployeeServiceRef.EmployeeServiceClient();
+            Employee employee = esc.GetEmployee(_testId);
+            esc.DeleteEmployee(employee);
+            employee = esc.GetEmployee(_testId);
+            Assert.IsNull(employee);
+        }
+    }
+}
